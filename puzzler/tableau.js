4d81564f5bc227aaaf7106067f18e0dab7e1da20
L Caraco <lbc.dna@gmail.com>
2021-03-20 16:00:09 -0500

    started work in the puzzler

---
+++ puzzler/tableau.js
@@ -0,0 +1,14 @@
+class Tableau {
+    constructor(x, y, shape) {
+        this.pos = [x, y];
+        this.shape = shape;
+    }
+
+    draw(shadow=false) {
+        
+    }
+}
+
+class TableauControl {
+
+}
