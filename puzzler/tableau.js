57b041ebcfcf739a800db42bc5f3f3d66278a013
L Caraco <lbc.dna@gmail.com>
2021-03-20 16:14:39 -0500

    added to utils

--- puzzler/tableau.js
+++ puzzler/tableau.js
@@ -2,13 +2,29 @@ class Tableau {
     constructor(x, y, shape) {
         this.pos = [x, y];
         this.shape = shape;
+        this.labels = Array.range(Array.sum(this.shape))
     }
 
     draw(shadow=false) {
         
     }
+
+    clone() {
+        const t = Tableau(this.pos[0], this.pos[1], this.shape); //.copy
+        t.labels = this.labels; //.copy
+        return t;
+    }
 }
 
-class TableauControl {
+// cols first, rows second.
 
+class TableauControl extends Tableau {
+    constructor(x, y, shape) {
+        this.pos = [x, y];
+        this.shape = shape;
+    }
+
+    draw() {
+
+    }
 }
