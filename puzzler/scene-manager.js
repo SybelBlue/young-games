de85da593a8ca0f7f055095848d543fb8b0a701f
L Caraco <lbc.dna@gmail.com>
2021-03-20 23:22:23 -0500

    added target

--- puzzler/scene-manager.js
+++ puzzler/scene-manager.js
@@ -2,14 +2,19 @@ class SceneManager {
     constructor(shape) {
         this.main = new Tableau(0, 0, shape);
         this.main.centerOn(windowWidth * 2 / 3, windowHeight / 3);
+        // scramble main
 
-        this.control = new TableauControl(0, 0, shape, console.log);
+        this.target = new Tableau(0, 0, shape);
+        this.target.centerOn(windowWidth - this.target.width / 2 - 4, windowHeight / 3)
+
+        this.control = new TableauControl(0, 0, shape, swap => this.main.swap(...swap));
         this.control.centerOn(windowWidth / 2, windowHeight * 2 / 3)
     }
 
     draw() {
         background(color("#276FBF"));
         this.main.draw();
+        this.target.draw();
         this.control.draw();
     }
 }
